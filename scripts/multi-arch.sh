138fd2250492de3c1a1dd7f297fa1de5fb68f246
Rafael Sene <rpsene@br.ibm.com>
2020-12-15 16:31:31 -0300

    Create multi-arch.sh

---
+++ scripts/multi-arch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+docker manifest create quay.io/powercloud/powervs-container-host:latest \
+quay.io/powercloud/powervs-container-host:x86_64 quay.io/powercloud/powervs-container-host:ppc64le
+
+docker manifest inspect quay.io/powercloud/powervs-container-host
+
+docker login quay.io -u $USER_QUAY -p $PWD_QUAY
+
+docker manifest push quay.io/powercloud/powervs-container-host:latest
