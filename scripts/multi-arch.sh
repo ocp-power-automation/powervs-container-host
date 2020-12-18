df773087649849b958df422430445d0992e0fd85
Rafael Sene <rpsene@br.ibm.com>
2020-12-18 17:15:59 -0300

    Update multi-arch.sh

--- scripts/multi-arch.sh
+++ scripts/multi-arch.sh
@@ -2,11 +2,13 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-docker manifest create quay.io/powercloud/powervs-container-host:latest \
+OCP_VERSION=latest
+
+docker manifest create quay.io/powercloud/powervs-container-host:$OCP_VERSION \
 quay.io/powercloud/powervs-container-host:container-host-x86_64 quay.io/powercloud/powervs-container-host:container-host-ppc64le
 
 #docker manifest inspect quay.io/powercloud/powervs-container-host
 
 docker login quay.io -u $USER_QUAY -p $PWD_QUAY
 
-docker manifest push quay.io/powercloud/powervs-container-host:latest
+docker manifest push quay.io/powercloud/powervs-container-host:$OCP_VERSION
