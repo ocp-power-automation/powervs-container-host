8e62c34ea9507e9d2b24880486b696bda91963a6
Rafael Peria de Sene <rpsene@br.ibm.com>
2021-03-01 15:19:10 -0300

    Update the main branch build steps
    Signed-off-by: Rafael Peria de Sene <rpsene@br.ibm.com>

--- scripts/multi-arch.sh
+++ scripts/multi-arch.sh
@@ -4,11 +4,11 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 OCP_VERSION=latest
 
-docker manifest create quay.io/powercloud/powervs-container-host:$OCP_VERSION \
-quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-x86_64 quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-ppc64le
+docker manifest create quay.io/powercloud/powervs-container-host:ocp-$OCP_VERSION \
+quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-x86_64 \ quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-ppc64le
 
-#docker manifest inspect quay.io/powercloud/powervs-container-host
+docker manifest inspect quay.io/powercloud/powervs-container-host:ocp-$OCP_VERSION
 
 docker login quay.io -u $USER_QUAY -p $PWD_QUAY
 
-docker manifest push quay.io/powercloud/powervs-container-host:$OCP_VERSION
+docker manifest push quay.io/powercloud/powervs-container-host:ocp-$OCP_VERSION
