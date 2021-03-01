0891dc9ab4cab5b1a319b93efdc006a4f82722e7
Rafael Sene <rpsene@br.ibm.com>
2021-03-01 15:33:04 -0300

    Update multi-arch.sh
    Fix wrongly set new line

--- scripts/multi-arch.sh
+++ scripts/multi-arch.sh
@@ -2,10 +2,10 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-OCP_VERSION=4.7
+OCP_VERSION="4.7"
 
 docker manifest create quay.io/powercloud/powervs-container-host:ocp-$OCP_VERSION \
-quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-x86_64 \ quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-ppc64le
+quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-x86_64 quay.io/powercloud/powervs-container-host:container-host-$OCP_VERSION-ppc64le
 
 docker manifest inspect quay.io/powercloud/powervs-container-host:ocp-$OCP_VERSION
 
